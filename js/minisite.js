a73348f47bc944a12fce73819b0ed1d1311569d8
jackie <jackie198512@126.com>
2015-04-11 21:40:01 +0800

    fix scroll

--- js/minisite.js
+++ js/minisite.js
@@ -15,5 +15,30 @@ $(function () {
         $('.tab .body').hide();
         $($('.tab .body').get($(this).index())).show();
 
+    });
+
+    $(window).bind('scroll', function () {
+        var tab_top = $('.tab_top'), //60
+            list = $('.tab');
+
+        if (list.length == 0 || tab_top.length == 0) {
+            return !1;
+        }
+
+        var list_offset = list.offset(),
+            list_offset_top = list_offset.top
+            ;
+
+        scroll_top = $(window).scrollTop();
+
+        if (scroll_top > list_offset_top) {
+
+            list.addClass('active');
+        } else {
+            list.removeClass('active');
+        }
+
+
     })
+
 })
